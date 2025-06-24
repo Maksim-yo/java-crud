3fe685bb66075f2f9a6a0a70d741352c9dc2f64f
maksim-yo <lyzlov023@gmail.com>
2025-06-25 02:19:03 +1000

    Add specification for dynamic sql in employee table

--- src/main/java/com/crud_api/crud_app/repository/EmployeeRepository.java
+++ src/main/java/com/crud_api/crud_app/repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.crud_api.crud_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -9,7 +10,7 @@ import java.util.List;
 import com.crud_api.crud_app.model.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+public interface EmployeeRepository extends JpaRepository<Employee, UUID>, JpaSpecificationExecutor<Employee>  {
     List<Employee> findByFullName(String fullName);
 
     List<Employee> findByCategoryId(UUID categoryId);
